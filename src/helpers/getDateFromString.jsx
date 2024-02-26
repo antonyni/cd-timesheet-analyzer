e17bf195b497199c8693383b63229bd971eb441f
Antony <antonyni1010@gmail.com>
2024-02-26 02:58:48 -0500

    fixed date formatting

--- src/helpers/getDateFromString.jsx
+++ src/helpers/getDateFromString.jsx
@@ -1,8 +1,8 @@
 import * as DateHelper from 'date-fns'
 const getDateFromString = (dateString) => {
     const currentDate = new Date(dateString);
-    const startOfWeek = DateHelper.startOfWeek(currentDate);
-    const endOfWeek = DateHelper.endOfWeek(currentDate);
+    const startOfWeek = DateHelper.startOfWeek(currentDate, { weekStartsOn: 1 });
+    const endOfWeek = DateHelper.endOfWeek(currentDate, { weekStartsOn: 1 });
     return (startOfWeek.getMonth() + 1) + '-' + startOfWeek.getDate() + '-' + startOfWeek.getFullYear() + " to " +
         (endOfWeek.getMonth() + 1) + '-' + endOfWeek.getDate() + '-' + endOfWeek.getFullYear();
 }
